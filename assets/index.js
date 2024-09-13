fa507f8fa5acbf5bd22b9dded77d900194198b11
BRENO OLIVEIRA GOMES <BRENO6162@DIGITALCOLLEGE.LOCAL>
2024-09-12 21:56:08 -0300

    api github

---
+++ assets/index.js
@@ -0,0 +1,39 @@
+const users = [
+    { username: 'breno-oliveira98' },
+    { username: 'tiagolimar' },
+    { username: 'edmaralbneto' },
+    { username: 'angelolustosa' },
+    { username: 'Gustavo1701' },
+    { username: 'miguelalves10' },
+    { username: 'antoniowgaldino' },
+    { username: 'rafaeoTW4' },
+    { username: 'JoaoRoberto1' },
+    { username: 'Breno-arauj' }
+]
+
+const tbody = document.getElementById('tbody')
+
+function fetchGitHub() {
+    fetch(`https://api.github.com/users/breno-oliveira98`, { headers: { 'Authorization': `token API_TOKEN` } })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            tbody.innerHTML =
+                `
+            <tr>
+                <td><img src="${data.avatar_url}" style="width: 60px; height: 60px; border-radius: 50px;"></td>
+                <td>${data.name}</td>
+                <td>${data.login}</td>
+                <td>${data.public_repos}</td>
+                <td><a href="${data.url}">GitHub</a></td>
+            </tr>
+            `
+        })
+}
+
+fetchGitHub()
+
+
+
+
+
